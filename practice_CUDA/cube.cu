7a339e53562e4070de7b9020923c12772068dde3
Taehun_KIM <kimx2815@umn.edu>
2013-10-22 17:41:09 -0500

    code complete

--- practice_CUDA/cube.cu
+++ practice_CUDA/cube.cu
@@ -2,13 +2,15 @@
 
 __global__ void cube(float * d_out, float * d_in){
 	// Todo: Fill in this function
-
+	int idx = threadIdx.x;
+	float f = d_in[idx];
+	d_out[idx] = f*f*f;
 
     
 }
 
 int main(int argc, char ** argv) {
-	const int ARRAY_SIZE = 64;
+	const int ARRAY_SIZE = 96;
 	const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
 
 	// generate the input array on the host
