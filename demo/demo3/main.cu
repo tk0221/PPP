d6083b9515615aba43c399cf8702587fa8ffec8b
Taehun Kim <kimx2815@umn.edu>
2013-12-11 05:47:20 -0600

    Update main.cu

--- demo/demo3/main.cu
+++ demo/demo3/main.cu
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
-__global__ void add(int * d_out, int * d_in){
+__global__ void cube(float * d_out, float * d_in){
 	
     int idx = threadIdx.x;
     int f = d_in[idx];
-    d_out[idx]=f+f;
+    d_out[idx]=f*f*f;
 
     
-} //
-
+} 
 int main(int argc, char ** argv) {
 	const int ARRAY_SIZE = 64;
-	const int ARRAY_BYTES = ARRAY_SIZE * sizeof(int);
+	const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
 
 	// generate the input array on the host
-	int h_in[ARRAY_SIZE];
+	float h_in[ARRAY_SIZE];
 	for (int i = 0; i < ARRAY_SIZE; i++) {
-		h_in[i] = i;
+		h_in[i] = float(i);
 	}
-	int h_out[ARRAY_SIZE];
+	float h_out[ARRAY_SIZE];
 
 	// declare GPU memory pointers
-	int * d_in;
-	int * d_out;
+	float * d_in;
+	float * d_out;
 
 	// allocate GPU memory
 	cudaMalloc((void**) &d_in, ARRAY_BYTES);
@@ -32,14 +31,14 @@ int main(int argc, char ** argv) {
 	cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
 
 	// launch the kernel
-	add<<<1, ARRAY_SIZE>>>(d_out, d_in);
+	cube<<<1, ARRAY_SIZE>>>(d_out, d_in);
 
 	// copy back the result array to the CPU
 	cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
 
 	// print out the resulting array
 	for (int i =0; i < ARRAY_SIZE; i++) {
-		printf("%d", h_out[i]);
+		printf("%f", h_out[i]);
 		printf(((i % 4) != 3) ? "\t" : "\n");
 	}
 
